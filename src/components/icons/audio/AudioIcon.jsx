e24e9cddfbadf6ec60a2ca606050691d8c72d0ee
arthurkyambadde <arthurkyambadde9@gmail.com>
2023-04-22 00:05:13 +0300

    (feat): add application icon components

--- src/components/icons/audio/AudioIcon.jsx
+++ src/components/icons/audio/AudioIcon.jsx
@@ -12,8 +12,8 @@ function AudioIcon() {
       >
         <circle opacity="0.25" cx="37.5" cy="37.5" r="37.5" fill="#A445ED" />
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M29 27V48L50 37.5L29 27Z"
           fill="#A445ED"
         />
