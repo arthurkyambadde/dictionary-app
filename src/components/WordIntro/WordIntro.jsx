ae5b7c1754708f9e9cb6eb4c7f05623d5d3b4157
arthurkyambadde <arthurkyambadde9@gmail.com>
2023-04-24 23:12:48 +0300

    (feat): add WordIntro component

---
+++ src/components/WordIntro/WordIntro.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import AudioIcon from "../icons/audio/AudioIcon";
+import styles from "./styles.module.css";
+import { useFetch } from "../../hooks/useFetch";
+
+function WordIntro() {
+  const { data } = useFetch();
+  return (
+    <div className={styles.word_intro__wrapper}>
+      <div>
+        <h1 className={styles.word}>Text</h1>
+        <p className={styles.pronounciation}>text</p>
+      </div>
+      <div className={styles.audio_icon}>
+        <AudioIcon />
+      </div>
+    </div>
+  );
+}
+
+
+export default WordIntro;
