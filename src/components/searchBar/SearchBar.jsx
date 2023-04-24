7be7fd9a21850fc039dee4ed2dd8f945c101b84b
arthurkyambadde <arthurkyambadde9@gmail.com>
2023-04-24 23:12:19 +0300

    (feat): add searchBar component to application

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import SearchIcon from "../icons/search/SearchIcon";
 import styles from "./styles.module.css";
+import { useFetch } from "../../hooks/useFetch";
 
 function SearchBar() {
+  const { text, setText, searchWord } = useFetch();
+
   return (
     <div className={styles.searchBar_wrapper}>
-      <input type="text" />
-      <SearchIcon />
+      <input
+        value={text}
+        name="text"
+        onChange={(e) => setText(e.target.value)}
+        className={styles.input_area}
+        placeholder={"Enter Word"}
+      />
+      <div className={styles.search_icon} onClick={searchWord}>
+        <SearchIcon />
+      </div>
     </div>
   );
 }
