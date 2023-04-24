5aa6ac577b43d41dbe193d7e9433339ea4afb886
arthurkyambadde <arthurkyambadde9@gmail.com>
2023-04-24 23:14:31 +0300

    (styles): add application styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "./darkMode.css";
 import Header from "./components/header/Header";
 import styles from "./App.module.css";
 import SearchBar from "./components/searchBar/SearchBar";
+import WordIntro from "./components/WordIntro/WordIntro";
+import Meaning from "./components/meaning/Meaning";
+import { useFetch } from "./hooks/useFetch";
+import DataProvider from "./contexts/DataContext";
 
 function App() {
+  const { theme } = useFetch();
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
   return (
-    <div className={styles.application}>
-      <Header />
-      <SearchBar />
-    </div>
+    <DataProvider>
+      <div className={styles.application}>
+        <Header />
+        <SearchBar />
+        <WordIntro />
+        <Meaning />
+      </div>
+    </DataProvider>
   );
 }
 
