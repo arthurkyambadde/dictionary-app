6b0492c545b85b68475850f6bd3a564749ff4d19
arthurkyambadde <arthurkyambadde9@gmail.com>
2023-04-26 03:24:15 +0300

    (feat): add search functionality when error key is pressed

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -6,6 +6,12 @@ import { useFetch } from "../../hooks/useFetch";
 function SearchBar() {
   const { text, setText, searchWord } = useFetch();
 
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      searchWord();
+    }
+  };
+
   return (
     <div className={styles.searchBar_wrapper}>
       <input
@@ -14,6 +20,7 @@ function SearchBar() {
         onChange={(e) => setText(e.target.value)}
         className={styles.input_area}
         placeholder={"Enter Word"}
+        onKeyDown={handleKeyPress}
       />
       <div className={styles.search_icon} onClick={searchWord}>
         <SearchIcon />
